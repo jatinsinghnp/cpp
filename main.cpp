91efbd39ab39e5d3ad12041b8cb3bdb47cc5e1ca
jatinsinghnp <js8971105@gmail.com>
2022-11-08 22:27:46 +0545

    c++ begain

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+double square(int a){
+	
+  return a*a ;
+}
+void square1(int a){
+	
+	std::cout<<square(a)<<"\n";
+	
+}
+
+int x=0;
+
+int main()
+{
+std::cout<<"Hellow word"<<"\n";	
+std::cout<<"Hellow word"<<"\n";	
+
+square1(10);
+cout<<-x;
+}
