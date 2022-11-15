843cec716a0890fca359f56de209b862718bff7b
jatinsinghnp <js8971105@gmail.com>
2022-11-15 10:23:53 +0545

    void function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-
-double square(int a){
-	
-  return a*a ;
-}
-void square1(int a){
-	
-	std::cout<<square(a)<<"\n";
-	
+#include <iostream>
+// using namespace std; 
+//using driective 
+double pow(double  base,int exponent){
+  double result=1;
+  for (int i = 0; i < exponent;i++)
+  {
+  	result *=base;
+  }
+  return result;
 }
 
-int x=0;
 
-int main()
-{
-std::cout<<"Hellow word"<<"\n";	
-std::cout<<"Hellow word"<<"\n";	
+void print_pow(double base,int exponent){
+	
+	double myPower=pow(base,exponent);
+	std::cout<<base<<"raise to"<<exponent<<"power is"<<myPower<<std::endl;
+}
 
-square1(10);
-cout<<-x;
+
+
+int main(){ 
+    int base,exponent;
+    std::cout<<"What is the base :"<<std::endl;
+    std::cin>>base;
+    std::cout<<"what is the  exponent"<<std::endl;
+    std::cin>>exponent;
+
+    print_pow(base,exponent);
+	return 0;	    
 }
