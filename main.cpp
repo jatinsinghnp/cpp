568871b50dce7278055c6e3bd535e8fd81114de5
jatinsinghnp <js8971105@gmail.com>
2022-11-15 10:54:26 +0545

    all about char

--- main.cpp
+++ main.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
 #include <climits>
+using std::cout;
 
 int main(){ 
-	 short a; //16 ->  1 byte  = 8 bit     
-	 int b;  //32 ->   1 byte  = 8 bit
-	 long c; // 32 ->  1 byte  = 8 bit
-	 long long d; //64 -> 1 byte  = 8 bit 
+	 char x='A';
+	 char b='B';
+	 unsigned char c=200;
+	 cout<<(int)x<<std::endl;
+	 cout<<(int)b<<std::endl;
+	 cout<<(int)c<<std::endl;
+	 cout<<c<<std::endl;
 	 
-	 // short<=int<=long<=long long
 	 
-	 unsigned short aa;
-	 unsigned int bb;
-	 unsigned long cc;
-	 unsigned long long dd;
 	 
-	  // std::cout<<SHRT_MAX<<std::endl;
-	  // std::cout<<SHRT_MIN<<std::endl;
-	  // std::cout<<USHRT_MAX <<std::endl;
-	  // std::cout<<SHRT_MIN<<std::endl;
-	  
-	  std::cout<<ULLONG_MAX<<std::endl;
-	 
-   
 }
