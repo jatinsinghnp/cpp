71ac66e58c34f74e8a153e8b4e708658f47d0be2
jatinsinghnp <js8971105@gmail.com>
2022-11-17 16:09:44 +0545

    all about string

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <float.h>
+#include <string>
 using std::cout;
 using std::cin;
-
 int main(){ 
-	float a=10.0/3;
-	a=a*10000000000000000;
-	double b=7.7E4; //7.7E4
-		long double c;
-	cout<<b;
-	cout<<std::fixed<<a<<std::endl;
-	cout<<DBL_DIG<<std::endl;
-	cout<<FLT_DIG<<std::endl;
-	cout<<LDBL_DIG<<std::endl;
+	std::string greeting;
+	int a ;
+    getline(cin,greeting); //this is for getting string 
+    cin.getline(a);
+    
+    cout<<greeting;
+    
+    cout<<a;
 }
